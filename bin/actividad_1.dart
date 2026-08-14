43b41193bc2640387d2c825b5a231d7e4b9b2c6f
mxplz27 <palencialopez@gmail.com>
2026-08-13 19:15:17 -0500

    feat: agregar menu principal

--- bin/actividad_1.dart
+++ bin/actividad_1.dart
@@ -1,5 +1,46 @@
-import 'package:actividad_1/actividad_1.dart' as actividad_1;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${actividad_1.calculate()}!');
+void main() {
+  List<Map<String, dynamic>> productos = [];
+
+  bool continuar = true;
+
+  while (continuar) {
+    print('\n===== CATÁLOGO DE PRODUCTOS =====');
+    print('1. Agregar producto');
+    print('2. Listar productos');
+    print('3. Actualizar producto');
+    print('4. Eliminar producto');
+    print('5. Salir');
+    print('=================================');
+
+    stdout.write('Seleccione una opción: ');
+    String? opcion = stdin.readLineSync();
+
+    switch (opcion) {
+      case '1':
+        print('\nAgregar producto');
+        break;
+
+      case '2':
+        print('\nListar productos');
+        break;
+
+      case '3':
+        print('\nActualizar producto');
+        break;
+
+      case '4':
+        print('\nEliminar producto');
+        break;
+
+      case '5':
+        continuar = false;
+        print('\nPrograma finalizado.');
+        break;
+
+      default:
+        print('\nOpción inválida.');
+    }
+  }
 }
