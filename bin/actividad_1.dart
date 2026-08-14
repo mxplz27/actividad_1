7c9635a39e53aa55c6c1d44fb849ca77d5878c92
mxplz27 <palencialopez@gmail.com>
2026-08-13 19:11:38 -0500

    chore: actividad de productos

---
+++ bin/actividad_1.dart
@@ -0,0 +1,5 @@
+import 'package:actividad_1/actividad_1.dart' as actividad_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${actividad_1.calculate()}!');
+}
---
+++ test/actividad_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:actividad_1/actividad_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
